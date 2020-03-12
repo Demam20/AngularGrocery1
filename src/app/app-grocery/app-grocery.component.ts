5b03e95eaab441bdd2b8aedda41fcd0695fd3fcd
Demam20 <59580953+Demam20@users.noreply.github.com>
2020-03-11 18:39:35 -0700

    part2

--- src/app/app-grocery/app-grocery.component.ts
+++ src/app/app-grocery/app-grocery.component.ts
@@ -11,12 +11,52 @@ import { Component, OnInit } from '@angular/core';
 export class AppGroceryComponent {
 
      
-      task: string;
-      tasks = [];
-        onClick(){
-          this.tasks.push({name: this.task});
-          this.task = '';
       
+      task = {
+        name: '',
+        id: 0
+      };
+      tasks = [];
+      
+                                 
+      onClick(){
+        this.tasks.push({name: this.task , id:this.task});
+          
+          if(this.task.id == 0){
+            this.tasks.push({id: (new Date()).getTime(),name: this.task.name, strike: false });
+            } 
+            this.task = {
+              name: '',
+             id: 0
+            };
+            
+       
+       
   }  
+  onEdit(item){
+    this.task = item; 
+        
+    } 
+    onDelete(item){
+      for(var i = 0;i < this.tasks.length; i++){
+          if(item.id == this.tasks[i].id){
+              this.tasks.splice(i,1);
+              break;
+          }
+      }
+  }       
+  onStrike(item){
+    for(var i = 0;i < this.tasks.length; i++){
+      if(item.id == this.tasks[i].id){
+        if(this.tasks[i].strike){
+          this.tasks[i].strike = false;
+        }
+        else{
+          this.tasks[i].strike = true;
+        }
+        break;
+      }
+    }
+  }    
 
-}
+  }
